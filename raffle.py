1a6f910c332476d42d7668138e2a44d620f3828a
mceldp01 <polly.mceldowney@bbc.co.uk>
2018-11-16 11:02:29 +0000

    parse out the csv into a dict

--- raffle.py
+++ raffle.py
@@ -1,17 +1,33 @@
 import csv
+import random
+
+entries = []
+total_wedge = 0
 
 with open('DonationsList.csv') as csv_file:
 	#strip null bytes
-	csv_reader = csv.reader(x.replace('\0', '') for x in csv_file)
+	csv_dict = csv.DictReader(x.replace('\0', '') for x in csv_file)
 
 	#parse the csv
 	line_count = 0
-	for row in csv_reader:
-		print(row)
-		if line_count == 0:
-			print("Column names are %s" %(row))
-			line_count += 1
-		else:
-			print("\tname: %s email: %s" %(row[0], row[9]))
-			line_count += 1
-	print("Processed %s lines." %(line_count))
+	for row in csv_dict:
+		name = row['Display name']
+		amount = float(row['Amount'])
+		entries.append({'name': name, 'amount': amount})
+		total_wedge += amount
+
+print("total_wedge: %f" %(total_wedge))
+
+for row in entries:
+	print("\tname: %s amount: %s" %(row['name'], row['amount']))
+
+winning_number = random.randint(0, total_wedge)
+print("winning_number: %f" %(winning_number))
+
+
+
+#	current_person = ""
+#	current_money = 0
+#	for row in csv_dict:
+#		current_money += float(row['Amount'])
+#		current_person = row['Display name']
